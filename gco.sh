f2d497b7cf426922e33c1539c164a3aceddb3ce9
songjiayang <songjiayang1@gmail.com>
2017-10-19 16:43:54 +0800

    first commit

---
+++ gco.sh
@@ -0,0 +1,38 @@
+# !/bin/sh
+
+set -e
+if [ $# != 3 ]; then
+  echo "Usage:"
+
+  echo "gco example 0.1 main.go"
+  exit 1
+fi
+
+declare -a os=(
+"darwin" "darwin" "darwin" "darwin"
+"dragonfly"
+"freebsd" "freebsd" "freebsd"
+"linux" "linux" "linux" "linux" "linux" "linux" "linux" "linux" "linux" "linux"
+"netbsd" "netbsd" "netbsd"
+"openbsd" "openbsd" "openbsd"
+"plan9" "plan9"
+"solaris"
+"windows" "windows")
+
+declare -a arches=("386" "amd64" "arm" "arm64"
+"amd64"
+"386" "amd64" "arm"
+"386" "amd64" "arm" "arm64" "ppc64" "ppc64le" "mips" "mipsle" "mips64" "mips64le"
+"386" "amd64" "arm"
+"386" "amd64" "arm"
+"386" "amd64"
+"amd64"
+"386" "amd64")
+
+len=${#os[@]}
+
+for (( i=1; i<${len}+1; i++ ));
+do
+  echo "--> building pkg/$1-$2.${os[$i-1]}-${arches[$i-1]} $3"
+  go build -a -o "pkg/$1-$2.${os[$i-1]}-${arches[$i-1]}" $3
+done
